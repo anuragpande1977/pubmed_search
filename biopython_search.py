19d36fc1eac8649f6cca44c891d83678432efce3
anuragpande1977 <137955688+anuragpande1977@users.noreply.github.com>
2024-10-01 15:12:17 -0400

    biopython_search.py

--- biopython_search.py
+++ biopython_search.py
@@ -128,4 +128,11 @@ if st.button("Fetch Articles"):
         st.write("Please fill in all the required fields.")
 
 # Copyright information
-st.text("Software copyright holder: Anurag Pande")
+
+Copyright (c) 2024 Anurag Pande
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
+to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so.
+...
+
